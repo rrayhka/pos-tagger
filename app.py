0ae891416b881dfb524f592cc8b702f1cb6d7ca1
rrayhka <khyar075@gmail.com>
2024-07-01 19:05:57 +0700

    fix(app.py): fix some issues

--- app.py
+++ app.py
@@ -13,26 +13,15 @@ tokenizer = AutoTokenizer.from_pretrained(model_path)
 nlp = pipeline("token-classification", model=model, tokenizer=tokenizer)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def home(): return render_template('index.html')
 
 @app.route('/pos_tag', methods=['POST'])
 def pos_tag():
     text = request.json.get('text')
-    if not text:
-        return jsonify({"error": "No text provided"}), 400
-
-    logging.debug(f"Received text: {text}")
-
-    if isinstance(text, str):
-        text = text.encode('utf-8').decode('utf-8')
-
+    if not text: return jsonify({"error": "Masukkan Teks"}), 400
+    if isinstance(text, str): text = text.encode('utf-8').decode('utf-8')
     results = nlp(text)
-    tagged_tokens = [{"word": res["word"], "tag": res["entity"]} for res in results]
-
-    logging.debug(f"Tagged tokens: {tagged_tokens}")
-
+    tagged_tokens = [{"word": res["word"].replace('Ġ', ''), "tag": res["entity"].replace('B-', '').replace('I-', '')} for res in results]
     return jsonify({"tagged_tokens": tagged_tokens})
-
 if __name__ == '__main__':
     app.run(port=5007, debug=True)
